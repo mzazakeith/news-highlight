5829a6558213fc51fa8f5bb5680fac986e5e59b6
mzazakeith <mzazakeith@gmail.com>
2018-06-18 12:21:41 +0300

    additional bootstrap styling

--- tests/news_test.py
+++ tests/news_test.py
@@ -11,17 +11,17 @@ class NewsTest(unittest.TestCase):
         """
         Set up method that will run before every test
         """
-        self.sources = Sources(1234, 'Amazing news', 'Thrilling news', 'https://abcnews.go.com', 'Technology',
+        self.sources = Sources(1, 'Amazing news', 'Thrilling news', 'https://abcnews.go.com', 'Technology',
                              'UnitedStates', 'English')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.sources, Sources))
 
     def test_to_check_instance_variables(self):
-        self.assertEquals(self.sources.id, 'wired')
-        self.assertEquals(self.sources.name, 'wired News')
-        self.assertEquals(self.sources.description, 'tech news provider')
-        self.assertEquals(self.sources.url, 'wired.com')
+        self.assertEquals(self.sources.id, 'sports')
+        self.assertEquals(self.sources.name, 'bbc')
+        self.assertEquals(self.sources.description, 'sports news provider')
+        self.assertEquals(self.sources.url, 'bbc-sports.com')
         self.assertEquals(self.sources.category, 'tech')
         self.assertEquals(self.sources.country, 'U.S.A')
         self.assertEquals(self.sources.language, 'en')
