8783e64f8ad3c8b9bee84c513de1b22619f9d569
mzazakeith <mzazakeith@gmail.com>
2018-06-18 09:08:01 +0300

    updating the config.pyto new file structure

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 
@@ -26,3 +29,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
