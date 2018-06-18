b97f4f90828c51d88bf03fdcc2d70ea80545d271
mzazakeith <mzazakeith@gmail.com>
2018-06-18 10:10:22 +0300

    added routes

--- app/main/views.py
+++ app/main/views.py
@@ -4,23 +4,23 @@ from ..request import get_sources,get_article
 from ..models import Sources
 
 
-# @app.route('/')
-# def index():
-#     """
-#     Function that returns the index page and all its data
-#     """
-#     sport_news = get_sources('sports')
-#     tech_news = get_sources('technology')
-#     title = 'ZIGS News-Breaking News,Latest News & Headlines'
-#     return render_template('index.html', title=title, sports=sport_news, technology=tech_news)
+@main.route('/')
+def index():
+    """
+    Function that returns the index page and all its data
+    """
+    sport_news = get_sources('sports')
+    tech_news = get_sources('technology')
+    title = 'ZIGS News-Breaking News,Latest News & Headlines'
+    return render_template('index.html', title=title, sports=sport_news, technology=tech_news)
+
 #
-#
-# @app.route('/source/<id>')
-# def articles(id):
-#     """
-#     View articles
-#     """
-#     article = get_article(id)
-#     print(article)
-#     title = f'ZIGS News ~ {id}'
-#     return render_template('news.html', title=title, article=article)
+@app.route('/source/<id>')
+def articles(id):
+    """
+    View articles
+    """
+    article = get_article(id)
+    print(article)
+    title = f'ZIGS News ~ {id}'
+    return render_template('news.html', title=title, article=article)
