2450d22ddb7f098a1a95359763212223c88d65ec
mzazakeith <mzazakeith@gmail.com>
2018-06-18 10:47:08 +0300

    configure request function added

--- app/requests.py
+++ app/requests.py
@@ -1,17 +1,25 @@
 import urllib.request, json
-from .models import news
+from .models import Sources,Article
 
-Sources = news.Sources
-Articles = news.Article
+# Sources = news.Sources
+Articles = Article
 
 # Getting the api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
 
 # Base URL
-base_url = app.config['NEWS_API_BASE_URL']
+base_url = None
 
 # Article URL
-article_url = app.config['BASE_URL']
+article_url = None
+
+
+def configure_request(app):
+    global api_key,base_url,article_url
+    api_key = app.config['NEWS_API_KEY']
+    print(api_key)
+    base_url = app.config['NEWS_API_BASE_URL']
+    article_url = app.config['BASE_URL']
 
 def get_sources(category):
     """
